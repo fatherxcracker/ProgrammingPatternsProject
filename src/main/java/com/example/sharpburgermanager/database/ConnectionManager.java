eef28a8f14bc2a80e1f915fceca137c226650670
James <jluciano0317@gmail.com>
2025-12-09 20:12:24 -0500

    Added config file

--- src/main/java/com/example/sharpburgermanager/database/ConnectionManager.java
+++ src/main/java/com/example/sharpburgermanager/database/ConnectionManager.java
@@ -1,12 +1,39 @@
 package com.example.sharpburgermanager.database;
 
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConnectionManager {
-    private static final String URL = "jdbc:mysql://localhost/sharpburger";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
+
+    private static String URL;
+    private static String USER;
+    private static String PASSWORD;
+
+    static {
+        try {
+            // Load XML from resources
+            InputStream is = ConnectionManager.class.getClassLoader()
+                    .getResourceAsStream("dbconfig.xml");
+            if (is == null) throw new RuntimeException("dbconfig.xml not found");
+
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(is);
+            doc.getDocumentElement().normalize();
+
+            URL = doc.getElementsByTagName("url").item(0).getTextContent();
+            USER = doc.getElementsByTagName("user").item(0).getTextContent();
+            PASSWORD = doc.getElementsByTagName("password").item(0).getTextContent();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static Connection getConnection() {
         try {
